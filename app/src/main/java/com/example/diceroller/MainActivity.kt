95fa79ea9d65582d669b293787550f8a318e1d5d
AsclepiosH91 <39828329+AsclepiosH91@users.noreply.github.com>
2019-05-27 20:39:41 +0200

    Second commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         //Use the findViewById() method to get a View reference for the view that you defined in the XML class.
         //In this case, you get the Button reference from the R class and the ID roll_button, and you assign that reference to the rollButton variable.
@@ -27,9 +32,16 @@ class MainActivity : AppCompatActivity() {
 
         val randomInt = Random().nextInt(6) + 1
 
-        //Toast.makeText(this, "button clicked",Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 
 
